eeb7622a245d78de97943c3da1a6a3c1fc2b943a
kevin.halimman <44279796+kevin-halimman@users.noreply.github.com>
2025-10-19 19:03:14 -0400

    added image upload button

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import tkinter as tk
 from tkinter import filedialog
-import cv2
+from tkinter import ttk
+from PIL import Image, ImageTk
 
 root = tk.Tk()
 root.title("Medical Imaging Edge Detection")
+root.geometry("800x600")
+
+def upload_image():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        image = Image.open(file_path)
+        image = image.resize((400, 400))
+        photo = ImageTk.PhotoImage(image)
+        label = ttk.Label(root, image=photo)
+        label.image = photo
+        label.pack()
+        
+ttk.Button(root, text = "Upload Image", command = upload_image).pack()
 root.mainloop()
