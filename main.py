8d4ac839bf47a8c3cfdefe931105e65cab05a649
kevin.halimman <44279796+kevin-halimman@users.noreply.github.com>
2025-10-19 22:38:49 -0400

    slightly working edge detection using guassian blur and canny algorithm

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import ttk
 from PIL import Image, ImageTk
+import cv2
 
 root = tk.Tk()
 root.title("Medical Imaging Edge Detection")
@@ -15,7 +16,17 @@ def upload_image():
         photo = ImageTk.PhotoImage(image)
         label = ttk.Label(root, image=photo)
         label.image = photo
+
+        image_cv = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+        blur = cv2.GaussianBlur(image_cv, (5, 5), 0)
+        edges = cv2.Canny(blur, 20, 210)
+        image2 = Image.fromarray(edges)
+        image2 = image2.resize((400, 400))
+        photo2 = ImageTk.PhotoImage(image2)
+        label2 = ttk.Label(root, image=photo2)
+        label2.image = photo2
+        label2.pack()
         label.pack()
-        
+
 ttk.Button(root, text = "Upload Image", command = upload_image).pack()
 root.mainloop()
