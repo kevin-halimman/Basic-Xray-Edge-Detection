4c2e2038e1bd128a455169637163537bd1355084
kevin.halimman <44279796+kevin-halimman@users.noreply.github.com>
2025-10-23 02:02:28 -0400

    added button to save processed image and updated README

--- README.md
+++ README.md
@@ -14,11 +14,13 @@ The goal of this project is to create a functional program that lets users uploa
 - Python 3.11
 - opencv
 - tkinter
+- PIL
 
 ## Usage
 Simply upload an image file to see a side-by-side comparison of the original and processed images. You can also use the two sliders to change the minimum and maximum threshold values for the edge-detection algorithm. It is important to note that the same parameters will not always work the best for different images. When you are satisfied with your processed image, you can save the file onto your machine. 
 
 ## Results
-
+Although the edge detection algorithm isn't robust and certainly doesn't meet industry standards by any means, it still serves its purpose as a basic edge detection program that lets you upload and save files. Upon completing the first version of this program, I can say I've learned a lot about the how the Canny edge detection algorithm works. I didn't implement it from scratch but I read up about the steps within the algorithm like noise reduction using a Gaussian blur function, gradient calculation, thresholding, etc. This definitely reinforced by understanding of how image processing worked mathematically and in the future, I would like to dive deeper into implementing an algorithm from scratch. Additionally, I was able to gain valuable experience creating a GUI using the tkinter library which is a skill that I will continute to utilize and improve in future projects.
 
 ## Structure
+I start off by defining a default window size of 1200x600 for the app. I've writen all my functions used in the program towards the top of the file so we can easily see what is happening behind the scenes. For instance, there are a few methods dedicated just to make sure that the scaling of the images and the Canny thresholds are updated in realtime based on the window size and the slider positions, respectively. After defining all my functions, I create a frame and divide it vertically into two equally-sized grids that will contain their respective images. The left side contains the original image and the right side contains the processed image. The rest of the code defines and packs the labels, sliders, and buttons you see in the program.
--- main.py
+++ main.py
@@ -44,6 +44,13 @@ def window_resize(event):
     if current_image_path:
         update_display()
 
+def save_file():
+    global processed_image
+    if processed_image:
+        file_path = filedialog.asksaveasfilename(defaultextension=".png",)
+        if file_path:
+            processed_image.save(file_path)
+
 def scale_to_fit(image, max_size):
     if image.width > max_size[0] or image.height > max_size[1]:
         ratio = min((max_size[0] - 30) / image.width, (max_size[1] - 30) / image.height)
@@ -61,7 +68,7 @@ def upload_image():
         image = Image.open(file_path)
         original_image = image.copy()
         photo = ImageTk.PhotoImage(image)
-        label_original.configure(image=photo, text = "Original Image", compound = 'bottom')
+        label_original.configure(image = photo, text = "Original Image", compound = 'bottom')
         label_original.image = photo
 
         image_cv = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
@@ -71,34 +78,35 @@ def upload_image():
         image2 = Image.fromarray(edges)
         photo2 = ImageTk.PhotoImage(image2)
         processed_image = image2.copy()
-        label_processed.configure(image=photo2, text = "Processed Image", compound = 'bottom')
+        label_processed.configure(image = photo2, text = "Processed Image", compound = 'bottom')
         label_processed.image = photo2
 
         update_display()
 
 imageframe = ttk.Frame(root)
-imageframe.pack(expand=True, fill='both')
+imageframe.pack(expand = True, fill = 'both')
 
-imageframe.columnconfigure(0, weight=1)
-imageframe.columnconfigure(1, weight=1)
+imageframe.columnconfigure(0, weight = 1)
+imageframe.columnconfigure(1, weight = 1)
 
-label_original = ttk.Label(imageframe, text="Original Image")
-label_processed = ttk.Label(imageframe, text="Processed Image")
-label_original.grid(row=0, column=0, padx=10, pady=10)
-label_processed.grid(row=0, column=1, padx=10, pady=10)
+label_original = ttk.Label(imageframe, text = "Original Image")
+label_processed = ttk.Label(imageframe, text = "Processed Image")
+label_original.grid(row = 0, column = 0, padx = 10, pady = 10)
+label_processed.grid(row = 0, column = 1, padx = 10, pady = 10)
 
-sliderlabel1 = ttk.Label(root, text="Canny Threshold 1")
-sliderlabel1.pack(padx=10, pady=5)
-slider1 = ttk.Scale(root, from_=0, to=300, orient='horizontal', length=300, command=update_image_processing)
+sliderlabel1 = ttk.Label(root, text = "Canny Threshold 1")
+sliderlabel1.pack(padx = 10, pady = 5)
+slider1 = ttk.Scale(root, from_ = 0, to = 300, orient='horizontal', length = 300, command = update_image_processing)
 slider1.set(30)
-slider1.pack(padx=10, pady=20)
-slider2 = ttk.Scale(root, from_=0, to=300, orient='horizontal', length=300, command=update_image_processing)
+slider1.pack(padx = 10, pady = 20)
+slider2 = ttk.Scale(root, from_ = 0, to = 300, orient='horizontal', length = 300, command = update_image_processing)
 slider2.set(150)
 sliderlabel2 = ttk.Label(root, text="Canny Threshold 2")
-sliderlabel2.pack(padx=10, pady=5)
-slider2.pack(padx=10, pady=5)
+sliderlabel2.pack(padx = 10, pady = 5)
+slider2.pack(padx = 10, pady = 5)
 
 ttk.Button(root, text = "Upload Image", command = upload_image).pack()
+ttk.Button(root, text = "Save Processed Image", command = save_file).pack()
 
 root.bind('<Configure>', window_resize)
 
